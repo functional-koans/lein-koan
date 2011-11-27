fb81aef11d56b8e20e41a322f138df5c2e539430
Sam Ritchie <sritchie09@gmail.com>
2011-11-27 14:17:43 -0500

    first commit>

---
+++ README.md
@@ -0,0 +1,13 @@
+# lein-koan
+
+Lein plugin for `koan-engine`.
+
+## Usage
+
+Add the following to your `project.clj` under `:dev-dependencies`:
+
+    [lein-koan "0.1.0"]
+
+## License
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ src/leiningen/koan.clj
@@ -0,0 +1,38 @@
+(ns leiningen.koan
+  (:refer-clojure :exclude (test))
+  (:use [leiningen.core :only (abort)]
+        [leiningen.help :only (help-for)]
+        [leiningen.compile :only (eval-in-project)]))
+
+(defn koan-help []
+  (help-for "koan"))
+
+(defn trigger-task [project task]
+  (eval-in-project project
+                   `(do (koan-engine.runner/exec ~task))
+                   nil nil
+                   `(do (require 'koan-engine.runner))))
+
+(defn run
+  "Doc string for run."
+  [project]
+  (trigger-task project "run"))
+
+(defn test
+  "Doc string for test."
+  [project]
+  (trigger-task project "test"))
+
+(defn koan
+  "Koan runner for Clojure projects."
+  {:help-arglists '([run test])
+   :subtasks [#'run #'test]}
+  ([project] (abort (koan-help)))
+  ([project subtask]
+     (case subtask
+       "help" (abort (koan-help))
+       "run"  (run project)
+       "test" (test project)
+       (abort (str subtask
+                   " is not a valid option. Run "
+                   "\"lein koan help\" to see your choices.")))))
