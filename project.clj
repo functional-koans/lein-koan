156e54d8a17977c5ea63e3d254c45a18f4641b16
Felix Breuer <felix@Shuvuhai.(none)>
2012-03-25 17:52:17 -0700

    bumped version to 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-koan "0.1.1-SNAPSHOT"
+(defproject lein-koan "0.1.2-SNAPSHOT"
   :description "Leiningen runner for the Clojure koan engine."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [koan-engine "0.1.1"]])
