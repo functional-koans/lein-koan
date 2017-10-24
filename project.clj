26af0965891d50d4f948ad05b7f622f542bd43cb
Colin Jones <trptcolin@gmail.com>
2017-10-24 11:22:11 -0500

    Bump koan-engine dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-koan "0.1.3"
+(defproject lein-koan "0.1.4"
   :description "Leiningen runner for the Clojure koan engine."
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [koan-engine "0.2.3"]])
+                 [koan-engine "0.2.4"]])
