677b2a2b6f13ef2c6f82dbecf9ade3e05249d194
Colin Jones <trptcolin@gmail.com>
2018-12-31 09:36:11 -0600

    Bump to latest Clojure

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-koan "0.1.4"
+(defproject lein-koan "0.1.5"
   :description "Leiningen runner for the Clojure koan engine."
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [koan-engine "0.2.4"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [koan-engine "0.2.5"]])
